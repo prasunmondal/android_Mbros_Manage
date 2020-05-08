64034dca3d417606e276e09d9aed62899e4a86a9
Prasun <pmandal@akamai.com>
2020-05-08 15:04:04 +0530

    otp generator

---
+++ app/src/main/java/com/prasunmondal/mbros_manage/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.prasunmondal.mbros_manage
+
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.ForegroundColorSpan
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.TextView
+
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+        setActionbarTextColor()
+
+        val array = arrayOf(2,3,5,7)
+        val otpBox = findViewById<TextView>(R.id.otpView)
+        var otp = 1
+        var prevOtp = 0
+        while(otp<1000000) {
+            prevOtp = otp
+            var random = rand(0,3)
+            otp *= array[random]
+        }
+        otpBox.text = prevOtp.toString()
+    }
+
+    fun rand(start: Int, end: Int): Int {
+        require(start <= end) { "Illegal Argument" }
+        return (Math.random() * (end - start + 1)).toInt() + start
+    }
+
+    private fun setActionbarTextColor() {
+        val title = "Generate OTP"
+        val spannableTitle: Spannable = SpannableString("")
+        spannableTitle.setSpan(
+            ForegroundColorSpan(Color.GRAY),
+            0,
+            spannableTitle.length,
+            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+        supportActionBar!!.title = title
+        window.statusBarColor = resources.getColor(R.color.sendMail_statusBar)
+        supportActionBar!!.setBackgroundDrawable(ColorDrawable(resources.getColor(R.color.sendMail_actionBar)))
+    }
+}
